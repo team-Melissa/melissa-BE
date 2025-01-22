4acde757f672848b61627f17607beb84c5e83afb
JSH <king601143@gmail.com>
2025-01-23 07:10:46 +0900

    feat: User 도메인, Converter, Repo 추가

---
+++ src/main/java/com/melissa/diary/domain/User.java
@@ -0,0 +1,43 @@
+package com.melissa.diary.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 50)
+    private String provider;
+
+    @Column(nullable = true, length = 100)
+    private String providerId;
+
+    @Column(nullable = false, length = 50)
+    private String nickname;
+
+    @Column(nullable = true, length = 255)
+    private String refreshToken;
+
+    private LocalDateTime refreshTokenExpiry;
+
+    @CreatedDate
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    private LocalDateTime updateAt;
+}
