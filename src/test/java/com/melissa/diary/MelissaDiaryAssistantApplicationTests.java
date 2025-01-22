db43094acf6128f8ad94592c27161e0d2605d610
JSH <king601143@gmail.com>
2025-01-22 17:26:05 +0900

    refactor: 프로젝트 계층형 구조 설계 #1

---
+++ src/test/java/com/melissa/diary/MelissaDiaryAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melissa.diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelissaDiaryAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
