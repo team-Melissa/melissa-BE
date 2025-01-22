6c0c5f58c0cb6dffa3934685f919d286859ca95d
JSH <king601143@gmail.com>
2025-01-23 06:17:54 +0900

    feat: security에 필요한 jwt Provider 및 Filter 설정

---
+++ src/main/java/com/melissa/diary/security/JwtProvider.java
@@ -0,0 +1,67 @@
+package com.melissa.diary.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    //@Value("${jwt.secret}")
+    private String secretKey = "asdasdasdsasssssssssssssssssssssssssssssssssssssssssssa";
+
+    // 유효 기간
+    private final long ACCESS_TOKEN_VALID_MILLIS = 1000L * 60 * 60;       // 1시간
+    private final long REFRESH_TOKEN_VALID_MILLIS = 1000L * 60 * 60 * 24; // 1일
+
+    // Access Token 생성
+    public String createAccessToken(Long userId, String provider) {
+        return createToken(userId, provider, ACCESS_TOKEN_VALID_MILLIS);
+    }
+
+    // Refresh Token 생성
+    public String createRefreshToken(Long userId, String provider) {
+        return createToken(userId, provider, REFRESH_TOKEN_VALID_MILLIS);
+    }
+
+    private String createToken(Long userId, String provider, long validMillis) {
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + validMillis);
+
+        return Jwts.builder()
+                .setSubject(String.valueOf(userId))
+                .claim("provider", provider)
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    // JWT 유효성 검증
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            // 만료 or 위조
+            return false;
+        }
+    }
+
+    // JWT에서 userId 추출
+    public Long getUserId(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+        return Long.valueOf(claims.getSubject());
+    }
+
+}
