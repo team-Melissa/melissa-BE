56d83924e0f5d11693f94d98cd71d5c920367681
JSH <king601143@gmail.com>
2025-01-26 02:14:49 +0900

    feat: Converter 구현

--- src/main/java/com/melissa/diary/converter/AiProfileConverter.java
+++ src/main/java/com/melissa/diary/converter/AiProfileConverter.java
@@ -3,14 +3,22 @@ package com.melissa.diary.converter;
 import com.melissa.diary.domain.AiProfile;
 import com.melissa.diary.domain.User;
 import com.melissa.diary.web.dto.AiProfileRequestDTO;
+import com.melissa.diary.web.dto.AiProfileResponseDTO;
 
 public class AiProfileConverter {
 
-    // Req DTO -> Entity
-    public static AiProfile toAiProfile(AiProfileRequestDTO.AiProfileCreateRequest request, User user){
-        return AiProfile.builder()
-                .
-
+    // Entity -> Response DTO
+    public static AiProfileResponseDTO.AiProfileResponse toResponse(AiProfile aiProfile){
+        return AiProfileResponseDTO.AiProfileResponse.builder()
+                .aiProfileId(aiProfile.getId())
+                .profileName(aiProfile.getProfileName())
+                .imageUrl(aiProfile.getImageS3())
+                .feature1(aiProfile.getFeature1())
+                .feature2(aiProfile.getFeature2())
+                .feature3(aiProfile.getFeature3())
+                .hashTag1(aiProfile.getHashTag1())
+                .hashTag2(aiProfile.getHashTag2())
+                .createdAt(aiProfile.getCreatedAt())
                 .build();
     }
 
