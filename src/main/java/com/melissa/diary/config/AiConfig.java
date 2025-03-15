a2bc5573b94f8d8792f2d50568bbf822998e84e3
Jsh <king601143@gmail.com>
2025-03-15 18:05:17 +0900

    fix : AiConfig 내 모든 빈들 최신 빌더패턴으로 수정

--- src/main/java/com/melissa/diary/config/AiConfig.java
+++ src/main/java/com/melissa/diary/config/AiConfig.java
@@ -28,15 +28,22 @@ public class AiConfig {
 
     @Bean
     ChatModel chatModel() {
-        return new OpenAiChatModel(new OpenAiApi(apiKey));
+        OpenAiApi api = OpenAiApi.builder()
+                .apiKey(apiKey)
+                .build();
+        return OpenAiChatModel.builder().openAiApi(api).build();
     }
 
     @Bean(name = "profileClient")
     ChatClient profileClient(){
-        OpenAiApi api = new OpenAiApi(apiKey);
+
         OpenAiChatOptions options = OpenAiChatOptions.builder()
                 .model(OpenAiApi.ChatModel.GPT_4_O_MINI)
                 .temperature(0.5)
+                .build();
+
+        OpenAiApi api = OpenAiApi.builder()
+                .apiKey(apiKey)
                 .build();
 
         String system = """
@@ -46,7 +53,7 @@ public class AiConfig {
                 - 해시태그는 핵심 특징을 함축적으로 표현해야 함
                 - 기계적이거나 형식적인 답변 대신 실제 사람 작성하는 것처럼 자연스럽게 응답할 것""";
 
-        return ChatClient.builder(new OpenAiChatModel(api, options))
+        return ChatClient.builder(OpenAiChatModel.builder().openAiApi(api).defaultOptions(options).build())
                 .defaultSystem(system)
                 .build();
     }
@@ -54,13 +61,15 @@ public class AiConfig {
 
     @Bean(name = "aiChatClient")
     ChatClient chatClient() {
-        OpenAiApi api = new OpenAiApi(apiKey);
+        OpenAiApi api = OpenAiApi.builder()
+                .apiKey(apiKey)
+                .build();
         OpenAiChatOptions options = OpenAiChatOptions.builder()
                 .model(OpenAiApi.ChatModel.GPT_4_O_MINI)
                 .temperature(0.5)
                 .build();
 
-        return ChatClient.builder(new OpenAiChatModel(api, options))
+        return ChatClient.builder(OpenAiChatModel.builder().openAiApi(api).defaultOptions(options).build())
                 .defaultSystem("사용자와 채팅을 나누면서, 일기를 작성할 정보를 추출하거나 공감해줘. 대답에서 해시태그는 사용하지마." +
                         "너는 다음의 성격을 지녔고 사용자와의 대화에서 해당 내용을 무조건적으로 지켜야해 " +
                         "기본성격 : {system}" +
@@ -76,7 +85,9 @@ public class AiConfig {
 
     @Bean(name = "summaryClient")
     ChatClient summaryClient(){
-        OpenAiApi api = new OpenAiApi(apiKey);
+        OpenAiApi api = OpenAiApi.builder()
+                .apiKey(apiKey)
+                .build();
         OpenAiChatOptions options = OpenAiChatOptions.builder()
                 .model(OpenAiApi.ChatModel.GPT_4_O_MINI)
                 .temperature(0.5)
@@ -90,7 +101,7 @@ public class AiConfig {
                 - 그림일기에 어울리는 제목과 내용을 작성하고, 그림을 상상할 수 있도록 상세한 묘사를 포함합니다.
                 - 주제에 맞는 해시태그를 추가하여 일기의 특징을 강조합니다.""";
 
-        return ChatClient.builder(new OpenAiChatModel(api, options))
+        return ChatClient.builder(OpenAiChatModel.builder().openAiApi(api).defaultOptions(options).build())
                 .defaultSystem(system)
                 .build();
     }
