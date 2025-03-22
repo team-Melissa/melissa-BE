a1d4a8f2a18fb8de8b0353e4fc1d36002f0bce4d
JSH <king601143@gmail.com>
2025-03-23 01:10:10 +0900

    fix: 이미지 생성 사이즈 1024로 롤백

--- src/main/java/com/melissa/diary/ai/ImageGenerator.java
+++ src/main/java/com/melissa/diary/ai/ImageGenerator.java
@@ -28,8 +28,8 @@ public class ImageGenerator {
                 .builder()
                 .model("dall-e-3")
                 .style("natural")
-                .withHeight(512)
-                .withWidth(512)
+                .withHeight(1024)
+                .withWidth(1024)
                 .responseFormat("b64_json")
                 .build();
         ImagePrompt imagePrompt = new ImagePrompt(prompt, imageOptions);
